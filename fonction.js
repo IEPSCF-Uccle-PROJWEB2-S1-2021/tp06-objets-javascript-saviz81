de1362c3453b571783fa3c6596fed1598449eba7
Saviz yahyazadeh <yasa2103@student.iepscf-uccle.be>
2020-12-09 09:30:03 +0100

    TP06.ex1

--- fonction.js
+++ fonction.js
@@ -3,3 +3,11 @@ function Shape(name, sides, sideLength) {
   this.sides = sides;
   this.sideLength = sideLength;
 }
+Shape.prototype.calcPerimeter= function()
+{
+return this.sides*this.sideLength;
+}
+let square=new Shape("square",4,5);
+console.log(square.calcPerimeter());
+let triangle=new Shape("triangle",3,3);
+console.log(triangle.calcPerimeter());
